f48ec002bb6d18965dfa09ebed67e25270cf50b0
Aaron Velasco <avelasco292@gmail.com>
2023-05-03 16:01:11 -0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+//Modules
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alt-flavor')
+require('./7-mind-grenade')
+sayHi("susan");
+sayHi("john");
+sayHi("peter");
